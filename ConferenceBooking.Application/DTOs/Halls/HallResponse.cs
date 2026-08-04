1f6b1f0fac360ca71d44ee8da73443f4121b0b62
macedon-ps <63913255+macedon-ps@users.noreply.github.com>
2026-08-04 19:44:40 +0300

    the DTOs were created

---
+++ ConferenceBooking.Application/DTOs/Halls/HallResponse.cs
@@ -0,0 +1,34 @@
+﻿using ConferenceBooking.Application.DTOs.Services;
+
+namespace ConferenceBooking.Application.DTOs.Halls;
+
+/// <summary>
+/// Клас відповіді для отримання інформації про зал. Містить унікальний ідентифікатор, назву, місткість, погодинну ставку та список послуг.
+/// </summary>
+public class HallResponse
+{
+    /// <summary>
+    /// Guid Id залу. Містить унікальний ідентифікатор типу Guid, який використовується для ідентифікації конкретного залу.
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Назва залу. Містить рядок, який представляє назву залу. За замовчуванням встановлено порожній рядок.
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Місткість залу. Містить числове значення, яке вказує на максимальну кількість людей, які можуть перебувати у залі одночасно.
+    /// </summary>
+    public int Capacity { get; set; }
+
+    /// <summary>
+    /// Погодинна ставка залу. Містить числове значення типу decimal, яке представляє вартість оренди залу за годину.
+    /// </summary>
+    public decimal HourlyRate { get; set; }
+
+    /// <summary>
+    /// Список послуг, доступних у залі. Містить колекцію об'єктів типу ServiceResponse, які представляють різні послуги, що надаються разом із залом.
+    /// </summary>
+    public List<ServiceResponse> Services { get; set; } = new();
+}
---
+++ ConferenceBooking.Application/DTOs/Services/ServiceResponse.cs
@@ -0,0 +1,22 @@
+﻿namespace ConferenceBooking.Application.DTOs.Services;
+
+/// <summary>
+/// Клас відповіді для отримання інформації про послугу. Містить унікальний ідентифікатор, назву та ціну послуги.
+/// </summary>
+public class ServiceResponse
+{
+    /// <summary>
+    /// Guid Id послуги. Містить унікальний ідентифікатор типу Guid, який використовується для ідентифікації конкретної послуги.
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Назва послуги. Містить рядок, який представляє назву послуги. За замовчуванням встановлено порожній рядок.
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Ціна послуги. Містить числове значення типу decimal, яке представляє вартість послуги.
+    /// </summary>
+    public decimal Price { get; set; }
+}
